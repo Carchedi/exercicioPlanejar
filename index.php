6f8bd218655a4494ddb96ca254233b7183486545
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-07-25 19:24:19 -0300

    inteface inicial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Cálculo Horas de Trabalho</h3>
+    <br>
+    Hora inicial &nbsp;
+    <input type="time" id="inicial" name="inicial" min="00:00" max="23:59" required/>
+    <br>
+    Hora final &nbsp;
+    <input type="time" id="final" name="final" min="00:00" max="23:59" required/>
+    <br>
+      <input type="submit" value="Calcular"/>
+</body>
+</html>
