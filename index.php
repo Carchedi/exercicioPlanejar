2fa0c3d49c1fbc7996fcc70083c1f34cbc3b33e9
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-07-26 13:54:51 -0300

    Calculo de horas terminado, funcionando todos as hipoteses

--- index.php
+++ index.php
@@ -20,6 +20,19 @@
 </body>
 
 <?php
+    function formata_hora($tempo){
+        if(strlen($tempo) > 1){
+            $tempo = explode(":",$tempo);
+            if(strlen($tempo[0]) == 1){
+                $tempo[0] = "0".$tempo[0];
+            }
+            if(strlen($tempo[1]) == 1){
+                $tempo[1] = "0".$tempo[1];
+            }
+            return $tempo[0].":".$tempo[1];
+        }
+        return 0;
+    }
 
     function tempo_em_horas($horaA){  
         $horas = intdiv(strtotime($horaA), 3600);
@@ -44,16 +57,14 @@
     }
 
     function diferenca_tempo($horaA,$horaB){
-        
-        $datetime_1 = date('Y-m-d').$horaA; 
-        $datetime_2 = date('Y-m-d').$horaB;
-         
+        $datetime_1 = date('Y-m-d')." ".$horaA; 
+        $datetime_2 = date('Y-m-d')." ".$horaB;
 
         $start_datetime = new DateTime($datetime_1); 
         $end_datetime = new DateTime($datetime_2);
 
-        if($start_datetime > $end_datetime){
-        $end_datetime ->modify('+1 day');
+        if($start_datetime < $end_datetime){
+            $end_datetime ->modify('+1 day');
         }
 
         $diff = $start_datetime->diff($end_datetime); 
@@ -77,55 +88,46 @@
     $inicio = $_POST['inicial'];
     $fim = $_POST['final'];
 
-	// if (isset($inicio) and isset($fim)){  
-    //     $total_diurno = 0;
-    //     $total_noturno = 0;
+	if (isset($inicio) and isset($fim)){  
+        $total_diurno = 0;
+        $total_noturno = 0;
         
-    //     $periodo_inicio = get_periodo_horario($inicio);
-    //     $periodo_fim = get_periodo_horario($fim); 
+        $periodo_inicio = get_periodo_horario($inicio);
+        $periodo_fim = get_periodo_horario($fim); 
 
-    //     if($inicio < $fim){
-    //         if( $periodo_inicio == $periodo_fim){
-    //             if($periodo_inicio == 0){
-    //                 $total_diurno = diferenca_tempo($fim, $inicio);
-    //             }else{
-    //                 $total_noturno = diferenca_tempo($fim, $inicio);
-    //             }
-    //         }else{
-    //             $total_diurno = diferenca_tempo("22:00", $inicio);
-    //             if($fim < strtotime("22:00:00")){
-    //                $total_noturno = $fim + strtotime("02:00:00");
-    //             }else{
-    //                $total_noturno = diferenca_tempo($fim, "22:00");
-    //             }
-    //         }
-    //     }else{
-    //         if( $periodo_inicio == $periodo_fim){
-    //             if($periodo_inicio == 0){
-    //                 $total_diurno = (diferenca_tempo("22:00", $inicio)) + (diferenca_tempo($fim,"05:00"));
-    //                 $total_noturno = strtotime("07:00:00");
-    //             }else{
-    //                 $total_diurno = strtotime("17:00:00");
-    //                // $total_noturno = (diferenca_tempo("00:00"), $inicio) + (diferenca_tempo($fim, "22:00"));
-    //             }
-    //         }else{
-    //             if($periodo_inicio == 0){
-    //                 $total_diurno = diferenca_tempo("22:00", $inicio);;
-    //                 $total_noturno = strtotime("02:00:00") + $final;
-    //             }else{
-    //                 $total_diurno = $final - strtotime("05:00:00");
-    //                 $total_noturno = diferenca_tempo("29:00", $inicio);;
-    //             }
-    //         }
-    //     }
+        if($inicio < $fim){
+            if( $periodo_inicio == $periodo_fim){
+                if($periodo_inicio == 0){
+                    $total_diurno = diferenca_tempo($fim, $inicio);
+                }else{
+                    $total_noturno = diferenca_tempo($fim, $inicio);
+                }
+            }else{
+                $total_diurno = diferenca_tempo("22:00", $inicio);
+                $total_noturno = diferenca_tempo($fim, "22:00");
+            }
+        }else{
+            if( $periodo_inicio == $periodo_fim){
+                if($periodo_inicio == 0){
+                    $total_diurno = adiciona_tempo(diferenca_tempo("22:00", $inicio),diferenca_tempo($fim,"05:00"));
+                    $total_noturno = adiciona_tempo("00:00", "07:00");
+                }else{ 
+                    $total_noturno = adiciona_tempo(diferenca_tempo("24:00", $inicio),diferenca_tempo("00:00", $fim));
+                }
+            }else{
+                if($periodo_inicio == 0){
+                    $total_diurno = diferenca_tempo("22:00", $inicio);
+                    $total_noturno = adiciona_tempo("02:00", $fim);
+                }else{
+                    $total_diurno = diferenca_tempo($fim, "05:00");                    
+                    $total_noturno = adiciona_tempo(diferenca_tempo("24:00", $inicio),"05:00");
+                }
+            }
+        }
 
-    //     // echo "<br>".$inicio." -> ".$periodo_inicio."<br>".$fim." -> ".$periodo_fim;
-    //     echo "<br><br> Resultado:<br>Total horas diurnas: ".$total_diurno.
-    //          "<br>Total horas noturnas: ".$total_noturno;
-
-    
-    // }  
- 
+        echo "<br>Inicio: ".$inicio."     final: ".$fim;
+        echo "<br><br> Resultado:<br>Total horas diurnas: ".formata_hora($total_diurno).
+             "<br>Total horas noturnas: ".formata_hora($total_noturno);    
+    }  
 ?>
-
 </html>
