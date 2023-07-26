1a4257161febbe8c755c971a5526b7e0ab0bd015
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-07-25 21:34:25 -0300

    funcao que calcula a diferenca entre dois horarios

--- index.php
+++ index.php
@@ -8,12 +8,46 @@
 <body>
     <h3>Cálculo Horas de Trabalho</h3>
     <br>
-    Hora inicial &nbsp;
-    <input type="time" id="inicial" name="inicial" min="00:00" max="23:59" required/>
-    <br>
-    Hora final &nbsp;
-    <input type="time" id="final" name="final" min="00:00" max="23:59" required/>
-    <br>
-      <input type="submit" value="Calcular"/>
+    <form action="index.php" method="post">
+        Hora inicial &nbsp;
+        <input type="time" name="inicial" min="00:00" max="23:59" required/>
+        <br>
+        Hora final &nbsp;
+        <input type="time" name="final" min="00:00" max="23:59" required/>
+        <br>
+        <input type="submit" value="Calcular"/>
+    </form>
 </body>
+
+<?php
+
+    function tempo_trabalho($horaA, $horaB){
+        $ha = strtotime($horaA);
+        $hb = strtotime($horaB);
+        $segundos = $hb - $ha;
+        $hora = intdiv($segundos, 3600);
+        $segundos = $segundos - (3600 * $hora);
+        $minutos = intdiv($segundos, 60);
+
+        if($hora < 10 ){
+            $hora = "0".$hora;
+        }
+        if($minutos < 10 ){
+            $minutos = "0".$minutos;
+        }
+
+        return $hora.":".$minutos;
+    }
+
+
+    $inicio = $_POST['inicial'];
+    $fim = $_POST['final'];
+
+	if (isset($inicio) and isset($fim)){ 
+        echo $inicio."\t".$fim;
+        $tempo = tempo_trabalho($inicio, $fim);
+        echo "<br><br>".$tempo;
+    }    
+?>
+
 </html>
